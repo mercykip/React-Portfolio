895909e586cdc48fae0468d21d3dbb5542a6fad9
unknown <mercyjemosop@gmail.com>
2020-07-16 11:24:21 +0300

    Updates

---
+++ precache-manifest.c417e266d98392d5d306fc4fec933758.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e85b3bff73d3ba02d783d77919525c",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "656eb215dacecce8bdb0",
+    "url": "/React-Portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4e88acb91712b9d3d77d",
+    "url": "/React-Portfolio/static/css/main.dd5653d8.chunk.css"
+  },
+  {
+    "revision": "656eb215dacecce8bdb0",
+    "url": "/React-Portfolio/static/js/2.01d3c3ab.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/React-Portfolio/static/js/2.01d3c3ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e88acb91712b9d3d77d",
+    "url": "/React-Portfolio/static/js/main.11e7f0a8.chunk.js"
+  },
+  {
+    "revision": "89f5ed8e982227547873",
+    "url": "/React-Portfolio/static/js/runtime-main.3435375c.js"
+  }
+]);
